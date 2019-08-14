5d8187f3b37920c165723129575fcec50b27a3a0
João Lopes <joaopalosi@gmail.com>
2019-08-14 20:01:02 -0300

    Implementação de edição de cliente

--- src/main/java/application/controller/CustomerField.java
+++ src/main/java/application/controller/CustomerField.java
@@ -6,9 +6,11 @@ import application.view.auxiliary.Controller;
 import application.view.auxiliary.Formatter;
 import application.view.auxiliary.Window;
 import application.view.controller.CustomersController;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class CustomerField {
     private Customer customer;
@@ -17,7 +19,7 @@ public class CustomerField {
     private String name;
     private String document;
     private String phone;
-    private Button action = new Button();
+    private HBox action = new HBox();
 
     public CustomerField(Customer customer, Controller controller){
         this.customer = customer;
@@ -29,11 +31,23 @@ public class CustomerField {
 
         CustomersController customersController = (CustomersController) controller;
 
+        Button status = new Button();
         if(customer.isStatus()){
-            this.action.setMinSize(30, 30);
-            this.action.setMaxSize(30, 30);
-            this.action.getStyleClass().add("btnRed");
-            this.action.setOnAction(e -> {
+            Button edit = new Button();
+            edit.getStyleClass().add("btnBlueLight");
+            edit.setOnAction(e ->
+                    Window.changeScene(controller.getStage(), "editCustomer", controller, this.customer));
+            edit.setMinSize(30, 30);
+            edit.setMaxSize(30, 30);
+            ImageView imageEdit = new ImageView(new Image("/view/img/edit.png"));
+            imageEdit.setFitHeight(20);
+            imageEdit.setFitWidth(20);
+            edit.setGraphic(imageEdit);
+
+            status.setMinSize(30, 30);
+            status.setMaxSize(30, 30);
+            status.getStyleClass().add("btnRed");
+            status.setOnAction(e -> {
                 this.customer.setStatus(false);
                 if (CustomerModel.update(this.customer)) {
                     customersController.updateTable();
@@ -45,12 +59,14 @@ public class CustomerField {
             ImageView imageRemove = new ImageView(new Image("/view/img/active.png"));
             imageRemove.setFitHeight(20);
             imageRemove.setFitWidth(20);
-            this.action.setGraphic(imageRemove);
+            status.setGraphic(imageRemove);
+
+            this.action.getChildren().addAll(edit, status);
         } else {
-            this.action.setMinSize(30, 30);
-            this.action.setMaxSize(30, 30);
-            this.action.getStyleClass().add("btnGreen");
-            this.action.setOnAction(e -> {
+            status.setMinSize(30, 30);
+            status.setMaxSize(30, 30);
+            status.getStyleClass().add("btnGreen");
+            status.setOnAction(e -> {
                 this.customer.setStatus(true);
                 if (CustomerModel.update(this.customer)) {
                     customersController.updateTable();
@@ -62,9 +78,13 @@ public class CustomerField {
             ImageView imageRemove = new ImageView(new Image("/view/img/active.png"));
             imageRemove.setFitHeight(20);
             imageRemove.setFitWidth(20);
-            this.action.setGraphic(imageRemove);
+            status.setGraphic(imageRemove);
+
+            this.action.getChildren().add(status);
         }
 
+        this.action.setAlignment(Pos.CENTER_LEFT);
+        this.action.setSpacing(10);
     }
 
     public Customer getCustomer() {
@@ -107,11 +127,11 @@ public class CustomerField {
         this.phone = phone;
     }
 
-    public Button getAction() {
+    public HBox getAction() {
         return action;
     }
 
-    public void setAction(Button action) {
+    public void setAction(HBox action) {
         this.action = action;
     }
 }
---
+++ src/main/java/application/view/controller/EditCustomerController.java
@@ -0,0 +1,90 @@
+package application.view.controller;
+
+import application.controller.object.Customer;
+import application.model.CustomerModel;
+import application.view.auxiliary.*;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class EditCustomerController extends Controller {
+    private Customer customer;
+
+    @FXML private TextField txtName;
+    @FXML private TextField txtDocument;
+    @FXML private TextField txtPhone;
+
+    @FXML private AnchorPane waitScreen;
+
+    @Override
+    public void initialize(Stage oldStage, Scene scene, Controller oldController, Object... objects) {
+        this.customer = (Customer) objects[0];
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+            if(e.getCode() == KeyCode.ESCAPE){
+                cancel();
+                e.consume();
+            }
+        });
+
+        Window.setModal(this.stage, oldStage, oldController);
+        super.initialize(oldStage, scene, oldController, objects);
+
+        setupInputs();
+    }
+
+    private void setupInputs() {
+        Mask.upperCase(txtName);
+        Mask.document(txtDocument);
+        Mask.phone(txtPhone);
+
+        txtName.setText(this.customer.getName());
+        if(this.customer.getDocument() != null) txtDocument.setText(this.customer.getDocument());
+        txtPhone.setText(this.customer.getPhone());
+    }
+
+    @FXML private void save() {
+        if(Validator.isName(txtName.getText())){
+            this.customer.setName(txtName.getText());
+            if(txtDocument.getText().isEmpty() || Validator.isDocument(txtDocument.getText())){
+                if(!txtDocument.getText().isEmpty()){
+                    customer.setDocument(Formatter.unmaskOnlyNumber(txtDocument.getText()));
+                } else {
+                    customer.setDocument(null);
+                }
+                if(Validator.isPhone(txtPhone.getText())){
+                    customer.setPhone(Formatter.unmaskOnlyNumber(txtPhone.getText()));
+                    customer.setStatus(true);
+                    if(CustomerModel.update(customer)){
+                        this.stage.close();
+                    } else {
+                        Window.changeScene(this.stage, "error", this,
+                                "Não Foi Possivel Editar Cliente");
+                    }
+                } else {
+                    Window.changeScene(this.stage, "error", this,
+                            "Telefone Inválido");
+                }
+            } else {
+                Window.changeScene(this.stage, "error", this,
+                        "CPF Inválido");
+            }
+        } else {
+            Window.changeScene(this.stage, "error", this,
+                    "É Nescessario Nome e Sobrenome do Cliente");
+        }
+    }
+
+    @FXML private void cancel(){
+        this.stage.close();
+    }
+
+    @Override
+    public void activeWaitScreen(boolean wait) {
+        waitScreen.setVisible(wait);
+    }
+}
